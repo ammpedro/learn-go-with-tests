b66eee17fc99ac1a43d1bb02add497c1ad669534
Ana Pedro <pedro.ana.marian@gmail.com>
2021-01-23 17:40:39 +0800

    add convert to roman numerals tested upto 3999

--- roman-numerals/roman_numerals_test.go
+++ roman-numerals/roman_numerals_test.go
@@ -1,6 +1,9 @@
 package convert
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func assertConversion(t *testing.T, got, want string) {
 	t.Helper()
@@ -12,23 +15,47 @@ func assertConversion(t *testing.T, got, want string) {
 
 func TestRomanNumerals(t *testing.T) {
 	cases := []struct {
-		Description string
-		Arabic      int
-		Want        string
+		Arabic int
+		Roman  string
 	}{
-		{"1 should be I", 1, "I"},
-		{"2 should be II", 2, "II"},
-		{"3 should be III", 3, "III"},
-		{"4 should be IV", 4, "IV"},
-		{"5 should be V", 5, "V"},
+		{Arabic: 1, Roman: "I"},
+		{Arabic: 2, Roman: "II"},
+		{Arabic: 3, Roman: "III"},
+		{Arabic: 4, Roman: "IV"},
+		{Arabic: 5, Roman: "V"},
+		{Arabic: 6, Roman: "VI"},
+		{Arabic: 7, Roman: "VII"},
+		{Arabic: 8, Roman: "VIII"},
+		{Arabic: 9, Roman: "IX"},
+		{Arabic: 10, Roman: "X"},
+		{Arabic: 15, Roman: "XV"},
+		{Arabic: 18, Roman: "XVIII"},
+		{Arabic: 20, Roman: "XX"},
+		{Arabic: 24, Roman: "XXIV"},
+		{Arabic: 29, Roman: "XXIX"},
+		{Arabic: 35, Roman: "XXXV"},
+		{Arabic: 39, Roman: "XXXIX"},
+		{Arabic: 40, Roman: "XL"},
+		{Arabic: 44, Roman: "XLIV"},
+		{Arabic: 49, Roman: "XLIX"},
+		{Arabic: 50, Roman: "L"},
+		{Arabic: 53, Roman: "LIII"},
+		{Arabic: 60, Roman: "LX"},
+		{Arabic: 90, Roman: "XC"},
+		{Arabic: 150, Roman: "CL"},
+		{Arabic: 190, Roman: "CXC"},
+		{Arabic: 448, Roman: "LDXLVIII"},
+		{Arabic: 500, Roman: "D"},
+		{Arabic: 900, Roman: "CM"},
+		{Arabic: 1000, Roman: "M"},
+		{Arabic: 1984, Roman: "MCMLXXXIV"},
+		{Arabic: 2014, Roman: "MMXIV"},
+		{Arabic: 3999, Roman: "MMMCMXCIX"},
 	}
 
 	for _, test := range cases {
-		t.Run(test.Description, func(t *testing.T) {
-			got := ConvertToRoman(test.Arabic)
-			want := test.Want
-
-			assertConversion(t, got, want)
+		t.Run(fmt.Sprintf("%d should be %q", test.Arabic, test.Roman), func(t *testing.T) {
+			assertConversion(t, ConvertToRoman(test.Arabic), test.Roman)
 		})
 	}
 }
