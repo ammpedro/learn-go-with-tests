20478031ca27f9b397226b3296a158700d20ca88
Ana Pedro <pedro.ana.marian@gmail.com>
2021-01-26 20:24:57 +0800

    refactored mathclock for validating xml

--- mathclock/mathclock.go
+++ mathclock/mathclock.go
@@ -1,6 +1,8 @@
 package mathclock
 
 import (
+	"fmt"
+	"io"
 	"math"
 	"time"
 )
@@ -11,9 +13,42 @@ type Point struct {
 	Y float64
 }
 
-const secondHandLength = 90
-const clockCenterX = 150
-const clockCenterY = 150
+const (
+	secondHandLength = 90
+	clockCenterX     = 150
+	clockCenterY     = 150
+)
+
+//SVGWriter writes and SVG representation of an analog clock
+func SVGWriter(w io.Writer, t time.Time) {
+	io.WriteString(w, svgStart)
+	io.WriteString(w, bezel)
+	secondHand(w, t)
+	io.WriteString(w, svgEnd)
+}
+
+func secondHand(w io.Writer, t time.Time) {
+	p := secondHandPoint(t)
+	//scale for the secondhand
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength}
+	//flip, because the origin is in the top left
+	p = Point{p.X, -p.Y}
+	//tanslate, because the origin is in {150,150}
+	p = Point{p.X + clockCenterX, p.Y + clockCenterY}
+	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
+}
+
+const svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg xmlns="http://www.w3.org/2000/svg"
+     width="100%"
+     height="100%"
+     viewBox="0 0 300 300"
+     version="2.0">`
+
+const bezel = `<circle cx="150" cy="150" r="100" style="fill:#fff;stroke:#000;stroke-width:5px;"/>`
+
+const svgEnd = `</svg>`
 
 // SecondHand accepts time and returns the equivalent endpoint on the clock
 func SecondHand(t time.Time) Point {
--- mathclock/mathclock_test.go
+++ mathclock/mathclock_test.go
@@ -1,6 +1,8 @@
 package mathclock
 
 import (
+	"bytes"
+	"encoding/xml"
 	"math"
 	"testing"
 	"time"
@@ -87,4 +89,45 @@ func roughlyEqualFloat(a, b float64) bool {
 
 func roughlyEqualPoint(a, b Point) bool {
 	return roughlyEqualFloat(a.X, b.X) && roughlyEqualFloat(a.Y, b.Y)
+}
+
+type SVG struct {
+	XMLName xml.Name `xml:"svg"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Width   string   `xml:"width,attr"`
+	Height  string   `xml:"height,attr"`
+	ViewBox string   `xml:"viewBox,attr"`
+	Version string   `xml:"version,attr"`
+	Circle  Circle   `xml:"circle"`
+	Line    []Line   `xml:"line"`
+}
+type Circle struct {
+	Cx float64 `xml:"cx,attr"`
+	Cy float64 `xml:"cy,attr"`
+	R  float64 `xml:"r,attr"`
+}
+type Line struct {
+	X1 float64 `xml:"x1,attr"`
+	Y1 float64 `xml:"y1,attr"`
+	X2 float64 `xml:"x2,attr"`
+	Y2 float64 `xml:"y2,attr"`
+}
+
+func TestSVGWriterAtMidnight(t *testing.T) {
+	tm := time.Date(1234, time.January, 1, 0, 0, 0, 0, time.UTC)
+	b := bytes.Buffer{}
+	SVGWriter(&b, tm)
+
+	svg := SVG{}
+	xml.Unmarshal(b.Bytes(), &svg)
+
+	want := Line{150, 150, 150, 60}
+
+	for _, line := range svg.Line {
+		if line == want {
+			return
+		}
+	}
+
+	t.Errorf("Expected to find the secondhand line %+v in the SVG output %+v", want, svg.Line)
 }
